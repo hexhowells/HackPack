6b23d18689826ad628cad16619cad78c1b825499
Morgan <58524460+hexhowells@users.noreply.github.com>
2022-08-06 22:42:24 +0100

    add get_next_square

--- Python/math/square.py
+++ Python/math/square.py
@@ -7,3 +7,18 @@ def is_square(n: int) -> bool:
         n (int): value to check
     """
     return n == math.isqrt(n) ** 2
+
+
+def get_next_square(n: int) -> int:
+     """Gets next perfect square starting at int n (inclusive)
+        n=9 returns 9
+        n=10 returns 16
+        n=0 returns 0
+    
+    Args:
+        n (int): value to start from
+    """
+    if n <= 0:
+        return 0
+    else:
+        return (math.ceil(math.sqrt(n))) ** 2
